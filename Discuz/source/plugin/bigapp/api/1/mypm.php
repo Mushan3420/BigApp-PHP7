a02929babb89a4bc6a4e23095189074306f363b6
Mushan3420 <mushan3420@vip.qq.com>
2016-06-22 17:32:19 +0800

    Revert "Fix private message"
    This reverts commit ad567b7ce560ec752355441c1c4c0c6f8e97e001.

--- Discuz/source/plugin/bigapp/api/1/mypm.php
+++ Discuz/source/plugin/bigapp/api/1/mypm.php
@@ -12,14 +12,12 @@ if(!defined('IN_MOBILE_API')) {
 }
 
 
-class BigAppAPI
-{
-	function common()
-	{
+class BigAppAPI {
+
+	function common() {
 	}
 
-	function output()
-	{
+	function output() {
 		$search = array ("'<script[^>]*?>.*?</script>'si",
 				"'<img.*src=\"(.*)\" .*>'iU",
 				"'<a.*href=\"(.*)\".*target=\"_blank\">(.*)<\/a>'iU",
@@ -33,6 +31,7 @@ class BigAppAPI
 				"'&(cent|#162);'i",
 				"'&(pound|#163);'i",
 				"'&(copy|#169);'i",
+				"'&#(\d+);'e",
 				"'\\\\r\\\\n'i",
 				);
 
@@ -40,6 +39,7 @@ class BigAppAPI
 				"[img]\\1[/img]",
 				"[url]\\1[/url]",
 				"",
+				//"\\1",
 				"\"",
 				"&",
 				"<",
@@ -49,26 +49,20 @@ class BigAppAPI
 				chr(162),
 				chr(163),
 				chr(169),
+				"chr(\\1)",
 				"\r\n",
 				);		
 			
 		foreach ($GLOBALS['list'] as &$value){
-		if(isset($value['message'])){
+			if(isset($value['message'])){
 				$message = preg_replace ($search, $replace, $value['message']);	
-				$message = preg_replace_callback(
-					"/&#(\d+);/i",
-					function ($m) {
-						return chr($m[1]);
-					},
-					$message
-				);
 				if(function_exists('iconv')){
 					$message = iconv(CHARSET, 'UTF-8//ignore', $message);
 				}else{
 					$message = mb_convert_encoding($message, 'UTF-8', CHARSET);
 				}
 				$message = $ret = preg_replace_callback('/\[img](.*)\[\/img]/U', 'BigAppAPI::callback', $message);
-				
+               
 				$value['message'] = '__DONT_DICONV_TO_UTF8___' . $message;	
 			}
 			if(isset($value['msgtoid'])){
@@ -85,14 +79,14 @@ class BigAppAPI
 		unset($value);
 
 		$variable = array(
-			'list' => bigapp_core::getvalues($GLOBALS['list'], array('/^\d+$/'), array('plid', 'isnew', 'pmnum', 'lastupdate', 
-				'lastdateline', 'authorid', 'author', 'pmtype', 'subject', 'members', 'dateline', 'touid', 'pmid', 'lastauthorid', 
-				'lastauthor', 'lastsummary', 'msgfromid', 'msgfrom', 'message', 'msgtoid', 'msgtoid_avatar', 'msgfromid_avatar', 'tousername')),
-			'count' => $GLOBALS['count'],
-			'perpage' => $GLOBALS['perpage'],
-			'page' => intval($GLOBALS['page']),
-		);
-		
+            'list' => bigapp_core::getvalues($GLOBALS['list'], array('/^\d+$/'), array('plid', 'isnew', 'pmnum', 'lastupdate', 
+					'lastdateline', 'authorid', 'author', 'pmtype', 'subject', 'members', 'dateline', 'touid', 'pmid', 'lastauthorid', 
+					'lastauthor', 'lastsummary', 'msgfromid', 'msgfrom', 'message', 'msgtoid', 'msgtoid_avatar', 'msgfromid_avatar', 'tousername')),
+            'count' => $GLOBALS['count'],
+            'perpage' => $GLOBALS['perpage'],
+            'page' => intval($GLOBALS['page']),
+        );
+		 
 		$start = $variable['perpage'] * ($variable['page'] - 1);
 		$end = count($variable['list']) + $start;
 		if($end >= $variable['count']){
@@ -100,15 +94,14 @@ class BigAppAPI
 		}else{
 			$variable['need_more'] = 1;
 		}
-		if($_GET['subop']) {
-			$variable = array_merge($variable, array('pmid' => $GLOBALS['pmid']));
-		}
+        if($_GET['subop']) {
+            $variable = array_merge($variable, array('pmid' => $GLOBALS['pmid']));
+        }
 		$variable['list'] = array_values($variable['list']);
-		bigapp_core::result(bigapp_core::variable($variable));
+        bigapp_core::result(bigapp_core::variable($variable));
 	}
 
-	static function callback($matches)
-	{
+	static function callback($matches){
 		$smiles = array(
 			'smile.gif' => "\xF0\x9F\x98\x8C",
 			'sad.gif' => "\xF0\x9F\x98\x94",
@@ -142,20 +135,20 @@ class BigAppAPI
 		}
 		
 		$baseName = basename($matches[1]);
-				//baseName:smile.gif
-				$dir = "";
-				$url_info = parse_url($matches[1]);
-				if(isset($url_info['path'])) {
-					$array = explode('/', $url_info['path']);
-					
-					if(sizeof($array) - 2 >= 0) {
-						$dir = $array[sizeof($array) - 2];
-					} else {
-						$dir = "";
-					}
-				} else {
-					$dir = "";
-				}
+                //baseName:smile.gif
+                $dir = "";
+                $url_info = parse_url($matches[1]);
+                if(isset($url_info['path'])) {
+                  $array = explode('/', $url_info['path']);
+                  
+                  if(sizeof($array) - 2 >= 0) {
+                  	$dir = $array[sizeof($array) - 2];
+                  } else {
+                        $dir = "";
+                  }
+                } else {
+                  $dir = "";
+                }
 
 		$out = preg_match('/static\/image\/smiley/', $matches[1], $_matches);
 		
@@ -177,4 +170,5 @@ class BigAppAPI
 	}	
 
 }
+
 ?>
